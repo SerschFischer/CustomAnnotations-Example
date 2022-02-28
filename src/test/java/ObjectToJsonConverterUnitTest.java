9859695479d1f284f42f9d0d9fb9c187dfe09e3e
SerschFischer <sergej.knjasev@me.com>
2022-02-28 16:18:47 +0100

    custom annotions example finished

---
+++ src/test/java/ObjectToJsonConverterUnitTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+public class AnnotatedClassUnitTest {
+    @Test
+    void name() {
+        // GIVEN | ARRANGE
+        // WHEN | ACT
+        // THEN | ASSERT
+    }
+}
